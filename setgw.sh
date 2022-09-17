863f9c41947fdbeccf0b4d87df64b8110fdae336
root <root@radxa-zero>
2022-09-17 11:55:08 -0400

    updated

--- setgw.sh
+++ setgw.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 def=$(route | grep default)
 
-if [ -z "$def" ]; then
-        echo "default Route Missing - Adding it"
         ip=$(route | grep 255.255.255 | cut -d " " -f1)
         s1=$(echo "$ip" | cut -d "." -f1)
         s2=$(echo "$ip" | cut -d "." -f2)
@@ -10,9 +8,22 @@ if [ -z "$def" ]; then
         s4=1
         ss="$s1.$s2.$s3.$s4"
 	echo "New GW:$ss"
+
+if [ -z "$def" ]; then
+        echo "default Route Missing - Adding it"
         route add default gw "$ss" wlan0
         route
 else
         echo "Default Route Found - No Action"
 fi
+
+echo "Finding Active Connection"
+
+ Con=$(nmcli connection | grep wlan0 | cut -d " " -f1)
+echo "Con:$Con   IP:$ip    GW:$ss"
+echo  "Active Connection = $Con"
+
+
+
+sudo nmcli connection modify "$Con" ipv4.addresses "$ip"/24 ipv4.gateway "$ss"
 
